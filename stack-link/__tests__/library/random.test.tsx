6a00d6c53a3a4b645862dc59ff72237b99bed0a8
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2023-01-03 09:39:36 +0800

    Rewrote RNG to be a class

--- stack-link/__tests__/library/random.test.tsx
+++ stack-link/__tests__/library/random.test.tsx
@@ -1,4 +1,4 @@
-import { createRNG, randomIntegerBetween, randomPick } from "@/library/random";
+import RNG from "@/library/random";
 
 const SEED = 1672705995434;
 const LOOP_COUNT = 1000;
@@ -9,12 +9,13 @@ function loopPerform(operation: Function) {
   }
 }
 
-describe("random", () => {
-  describe("createRNG", () => {
+describe("RNG", () => {
+  const rng = new RNG(SEED);
+
+  describe("next", () => {
     it("should always return a value between [0, 1)", () => {
-      const rng = createRNG(SEED);
       loopPerform(() => {
-        const randomValue = rng();
+        const randomValue = rng.next();
         expect(randomValue).toBeGreaterThanOrEqual(0);
         expect(randomValue).toBeLessThan(1);
       });
@@ -22,14 +23,12 @@ describe("random", () => {
   });
 
   describe("randomIntegerBetween", () => {
-    const rng = createRNG(SEED);
-
     it("should always return a value between minimum and maximum", () => {
       const listLength = 25;
       const bitList = Array.from({length: listLength}, () => false);
       
       loopPerform(() => {
-        const randomValue = randomIntegerBetween(0, listLength, rng);
+        const randomValue = rng.randomIntegerBetween(0, listLength);
         expect(randomValue).toBeGreaterThanOrEqual(0);
         expect(randomValue).toBeLessThan(listLength);
         bitList[randomValue] = true;
@@ -40,14 +39,12 @@ describe("random", () => {
   });
 
   describe("randomPick", () => {
-    const rng = createRNG(SEED);
-
     it("should always return a value from the list", () => {
       const list = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
       const bitMap: any = {};
       
       loopPerform(() => {
-        const randomValue = randomPick(list, rng);
+        const randomValue = rng.randomPick(list);
         expect(list).toContain(randomValue);
         bitMap[randomValue] = true;
       });
--- stack-link/library/random.ts
+++ stack-link/library/random.ts
@@ -1,17 +1,25 @@
-// mulberry32
-export function createRNG(seed: number) {
-  return function(): number {
-    var t = seed += 0x6D2B79F5;
-    t = Math.imul(t ^ t >>> 15, t | 1);
-    t ^= t + Math.imul(t ^ t >>> 7, t | 61);
-    return ((t ^ t >>> 14) >>> 0) / 4294967296;
+export default class RNG {
+  private rng: () => number;
+
+  constructor(seed: number) {
+    // mulberry32
+    this.rng = function () {
+      var t = seed += 0x6D2B79F5;
+      t = Math.imul(t ^ t >>> 15, t | 1);
+      t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+      return ((t ^ t >>> 14) >>> 0) / 4294967296;
+    };
   }
-}
 
-export function randomIntegerBetween(min: number, max: number, rng: () => number) {
-  return min + Math.floor(rng() * (max - min));
-}
+  next() {
+    return this.rng();
+  }
 
-export function randomPick<T>(list: T[], rng: () => number): T {
-  return list[randomIntegerBetween(0, list.length, rng)];
+  randomIntegerBetween(min: number, max: number) {
+    return min + Math.floor(this.rng() * (max - min));
+  }
+
+  randomPick<T>(list: T[]): T {
+    return list[this.randomIntegerBetween(0, list.length)];
+  }
 }
