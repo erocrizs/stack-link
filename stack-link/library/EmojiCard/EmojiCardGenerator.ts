73545f4d8f6fc46ee2dbfde61079bfabe2f5546d
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2023-01-03 11:07:36 +0800

    Implement EmojiCardGenerator

---
+++ stack-link/library/EmojiCard/EmojiCardGenerator.ts
@@ -0,0 +1,24 @@
+import RNG from "../RNG";
+import { Emoji, emojis } from "../emoji";
+import EmojiCard from "./EmojiCard";
+
+export default function* EmojiCardGenerator(size: number, seed?: number): Generator<EmojiCard, EmojiCard, undefined> {
+  const rng = new RNG(seed || Date.now());
+
+  const emojiState: Emoji[] = [];
+  
+  while (size-- > 1) {
+    while (emojiState.length < 17) {
+      const newEmoji = rng.randomPick<Emoji>(emojis);
+      if (!emojiState.includes(newEmoji)) {
+        emojiState.push(newEmoji);
+      }
+    }
+
+    const newEmojiCard = emojiState.splice(0, 8);
+    newEmojiCard.push(emojiState[0]);
+    yield new EmojiCard(newEmojiCard);
+  }
+
+  return new EmojiCard(emojiState);
+}
